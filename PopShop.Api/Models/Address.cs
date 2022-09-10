05a3276eb008cf85d592046fed44b02034f2a95e
barretoareias <barretoareias@gmail.com>
2022-09-09 21:14:29 -0300

    Added Postal Code Endpoint

---
+++ PopShop.Api/Models/Address.cs
@@ -0,0 +1,11 @@
+namespace PopShop.Api.Models;
+
+public class Address
+{
+	public string PostalCode { get; set; }
+	public string PublicArea { get; set; }
+	public string Complement { get; set; }
+	public string Distric { get; set; }
+	public string City { get; set; }
+	public string State { get; set; }
+}
