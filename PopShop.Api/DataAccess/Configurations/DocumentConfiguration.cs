ff69c3dc7ff08898d31855bd1df62544dd2f1f4c
unknown <dirceu.c.pereira@gmail.com>
2022-09-16 19:35:37 -0300

    add config banco

---
+++ PopShop.Api/DataAccess/Configurations/DocumentConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Metadata;
+
+namespace PopShop.Api.DataAccess.Configurations
+{
+   /* public class DocumentConfiguration : IEntityTypeConfiguration<Document>
+    {
+   
+        }
+    }*/
+}
---
+++ PopShop.Api/DataAccess/Configurations/StoreConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PopShop.Api.DataContract;
+
+namespace PopShop.Api.DataAccess.Configurations
+{
+    public class StoreConfiguration : IEntityTypeConfiguration<Store>
+    {
+        public void Configure(EntityTypeBuilder<Store> builder)
+        {
+            builder.ToTable("Loja", DataContext.SCHEMA)
+                .HasKey(e => e.Id)
+                .HasName("Id");
+
+            builder.Property(e => e.Name)
+                .HasColumnName("Nome")
+                .IsRequired()
+                .HasMaxLength(60);
+
+            builder.Property(e => e.Description)
+                .HasColumnName("Descricao")
+                .IsRequired()
+                .HasMaxLength(80);
+
+            builder.HasKey(e => e.PhotoUrl)
+                .HasName("FotoUrl");
+
+        }
+    }
+}
