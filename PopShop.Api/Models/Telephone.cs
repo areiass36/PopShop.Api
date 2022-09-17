a43d2b02c1f801c151f5b6a40d5cc328e9d62632
unknown <dirceu.c.pereira@gmail.com>
2022-09-17 14:24:00 -0300

    add novas config's banco

---
+++ PopShop.Api/Models/Telephone.cs
@@ -0,0 +1,10 @@
+﻿namespace PopShop.Api.Models
+{
+    public class Telephone
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Ddd {get; set;}
+        public string Numero {get; set;}
+    }
+}
